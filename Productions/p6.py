11d115902b25d055c986cf4e838b02dae85073eb
KubaBiz <1kubi1@wp.pl>
2023-01-05 11:56:38 +0100

    p6

--- Productions/p6.py
+++ Productions/p6.py
@@ -1,5 +1,5 @@
 import igraph as ig
-import production as production
+import Productions.production as production
 
 # Nie dodałem etykiet krawędzi, jak ustalimy jak chcemy to robić to dodam (tworzy puste krawędzie)
 
@@ -24,26 +24,26 @@ class P6(production.Production):
     def specification():
         return "L: A1 -> A2\nR: C <- A1 -> A2' -> B"
 
-a = ig.Graph([(0,1)])
-a.vs['Etykieta']=['A','A']
-print(len(a.vs))
-p6 = P6()
-p6.produce(a, [0,1])
-print(len(a.vs))
-print(a.get_edgelist())
-print(list(a.vs()))
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots(figsize=(5,5))
-ig.plot(
-    a,
-    target=ax,
-    layout="circle", # print nodes in a circular layout
-    vertex_size=0.1,
-    vertex_color="steelblue",
-    vertex_frame_width=4.0,
-    vertex_frame_color="white",
-    vertex_label=a.vs["Etykieta"],
-    vertex_label_size=7.0,
-    edge_width=2,
-    edge_color="#7142cf")
-plt.show()
+# a = ig.Graph([(0,1)])
+# a.vs['Etykieta']=['A','A']
+# print(len(a.vs))
+# p6 = P6()
+# p6.produce(a, [0,1])
+# print(len(a.vs))
+# print(a.get_edgelist())
+# print(list(a.vs()))
+# import matplotlib.pyplot as plt
+# fig, ax = plt.subplots(figsize=(5,5))
+# ig.plot(
+#     a,
+#     target=ax,
+#     layout="circle", # print nodes in a circular layout
+#     vertex_size=0.1,
+#     vertex_color="steelblue",
+#     vertex_frame_width=4.0,
+#     vertex_frame_color="white",
+#     vertex_label=a.vs["Etykieta"],
+#     vertex_label_size=7.0,
+#     edge_width=2,
+#     edge_color="#7142cf")
+# plt.show()
