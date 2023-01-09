62d7d1aee6084833a18dd00dbeb4a0dc5e65e337
KubaBiz <1kubi1@wp.pl>
2023-01-09 18:16:09 +0100

    p7

--- Productions/p7.py
+++ Productions/p7.py
@@ -1,4 +1,15 @@
 import igraph as ig
-import Productions.production
+import Productions.production as production
 
-#class P7(Productions.production.Production):
+class P7(production.Production):
+    @staticmethod
+    def produce(g: ig.Graph, array: list): # (A B) -> (A -> B) -- dowolne dwa wierzchołki są łączone 
+        if len(array) == 2 and max(array) < g.vcount() and not g.are_connected(array[0], array[1]):
+            g.add_edges([(array[0], array[1])])
+            
+    @staticmethod
+    def to_string():
+        return "Production 7"
+    @staticmethod
+    def specification():
+        return "L: A B\nR: A -> B"
